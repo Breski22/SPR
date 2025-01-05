cb8db2e28b3908ff6168aab2f84a956726c7cbf5
Breski22 <xwaw22@gmail.com>
2025-01-05 14:05:52 +0100

    Problem z zad1

---
+++ Zad1/Zad1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+string slowo;
+char litera;
+int i, ile;
+int main()
+{
+	cout << "Podaj slowo: ";
+	cin >> slowo;
+	cout << "Podaj szukana litere: ";
+	cin >> litera;
+	ile = 0;
+	for(i=0; i<slowo.size(); i++)
+		if (slowo[i] == litera)
+		{
+			ile++;
+		}
+	cout << "Litera " << litera << " wystepuje w slowie " << slowo << " " << ile << " razy";
+}
