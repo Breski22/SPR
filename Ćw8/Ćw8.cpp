394ea71000993c6979bbd2f51f593fafdb1f4bde
Breski22 <xwaw22@gmail.com>
2025-01-05 14:02:52 +0100

    Dodano ćw8

---
+++ Ćw8/Ćw8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>//liczba wystapien danej litery w napisie
+#include <string>
+using namespace std;
+string s;
+char c;
+int a=0;
+int main()
+{
+	cout << "Podaj wyraz: "; cin >> s;
+	cout << "Podaj litere: "; cin >> c;
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == c)
+		{
+			a++;
+		}
+	}
+	cout << "Liczba wystapien litery " << c << " w wyrazie " << s << " wynosi: " << a << endl;
+}
